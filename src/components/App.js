371f181e0eff090c10a87266e3037f196115b1cf
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-22 19:33:46 +1100

    view all listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  //set up state
+  const [listings, setListings] = useState([]);
+
+  //useEffect hook to fetch listings onMount
+  //update state
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(res => res.json())
+      .then(listingsData => {
+        setListings(listingsData);
+      })
+      .catch(err => console.warn(err.message))
+  }, []);
+
+  console.log(`Listings are ${listings}`);
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
